d0bddf657dfe71fc474251362ab0ef661d0b633e
sandeepnaga <sandeep.naga@flipkart.com>
2015-07-15 15:06:02 +0530

    added code for gravator and user model view page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,6 +21,7 @@ describe UsersController do
     end
 
     it "should be successful" do
+      puts "user is  #{@user}"
       get :show, :id => @user
       response.should be_success
     end
@@ -29,6 +30,22 @@ describe UsersController do
       get :show, :id => @user
       assigns(:user).should == @user
     end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+
   end
 
 end
